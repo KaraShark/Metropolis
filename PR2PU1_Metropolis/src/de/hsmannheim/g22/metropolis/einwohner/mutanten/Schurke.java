665e6e530a39f589a7c19b0446a2630cb74da629
Senai <1725950@stud.hs-mannheim.de>
2018-12-01 20:42:30 +0100

    Schurke und Superheld kaempfe-Methode kleine Verbesserung

--- PR2PU1_Metropolis/src/de/hsmannheim/g22/metropolis/einwohner/mutanten/Schurke.java
+++ PR2PU1_Metropolis/src/de/hsmannheim/g22/metropolis/einwohner/mutanten/Schurke.java
@@ -1,39 +1,37 @@
 package de.hsmannheim.g22.metropolis.einwohner.mutanten;
 
-public class Schurke extends Mutant{
+public class Schurke extends Mutant {
     private int einkommen;
     private String schwaeche;
-    
-    public Schurke(){}
-    
-    public Schurke(String name, String schwaeche, int einkommen){
-        //name
+
+    public Schurke() {
+    }
+
+    public Schurke(String name, String schwaeche, int einkommen) {
+        // name
         this.schwaeche = schwaeche;
         this.einkommen = einkommen;
-        
+
     }
-    
-    public boolean kaempfe(Superheld superheld){
-        boolean gewonnen = false;
-        
-        for(int i=0;i<superheld.getSuperkraefte().length;i++){
-            if(schwaeche.equals(superheld.getSuperkraefte())){
+
+    public boolean kaempfe(Superheld superheld) {
+        boolean gewonnen = true;
+
+        for (int i = 0; i < superheld.getSuperkraefte().length && gewonnen; i++) {
+            if (schwaeche.equals(superheld.getSuperkraefte()[i])) {
                 gewonnen = false;
             }
-            else{
-                gewonnen = true;
-            }
-            
+
         }
         return gewonnen;
     }
-    
-    public String getSchwaeche(){
+
+    public String getSchwaeche() {
         return this.schwaeche;
     }
-    
+
     public int getEinkommen() {
-		return einkommen;
-	}
-    
+        return einkommen;
+    }
+
 }
--- PR2PU1_Metropolis/src/de/hsmannheim/g22/metropolis/einwohner/mutanten/Superheld.java
+++ PR2PU1_Metropolis/src/de/hsmannheim/g22/metropolis/einwohner/mutanten/Superheld.java
@@ -1,31 +1,33 @@
 package de.hsmannheim.g22.metropolis.einwohner.mutanten;
 
-public class Superheld extends Mutant{
+public class Superheld extends Mutant {
     private String[] superkraefte;
-    
-    public Superheld(){}
-    public Superheld(String name, String[] superkraefte){
-        //name
-        this.superkraefte = superkraefte;    
+
+    public Superheld() {
     }
-    public boolean kaempfe(Schurke schurke){
+
+    public Superheld(String name, String[] superkraefte) {
+        // name
+        this.superkraefte = superkraefte;
+    }
+
+    public boolean kaempfe(Schurke schurke) {
         boolean gewonnen = false;
-        
-        for(int i=0;i<superkraefte.length;i++){
-            if(superkraefte[i].equals(schurke.getSchwaeche())){
-                gewonnen = false;
+
+        for (int i = 0; i < superkraefte.length; i++) {
+            if (superkraefte[i].equals(schurke.getSchwaeche())) {
+                gewonnen = true;
             }
-            gewonnen = true;
         }
         return gewonnen;
-        
+
     }
-    
-    //nicht ganz sicher
-    public String[] getSuperkraefte(){
-        for(int i=0;i<superkraefte.length;i++){
-           System.out.print(superkraefte[i]);
-        }
+
+    // nicht ganz sicher
+    public String[] getSuperkraefte() {
+        // for(int i=0;i<superkraefte.length;i++){
+        // System.out.print(superkraefte[i]);
+        // }
         return superkraefte;
     }
 }
