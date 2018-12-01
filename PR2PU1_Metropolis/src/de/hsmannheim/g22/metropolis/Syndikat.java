b0b357ad66cb2d118b34f43dd25e455f632356a8
Senai <1725950@stud.hs-mannheim.de>
2018-12-01 21:33:48 +0100

    .

--- PR2PU1_Metropolis/src/de/hsmannheim/g22/metropolis/Syndikat.java
+++ PR2PU1_Metropolis/src/de/hsmannheim/g22/metropolis/Syndikat.java
@@ -1,23 +1,21 @@
 package de.hsmannheim.g22.metropolis;
 
-import java.util.Arrays;
-
 import de.hsmannheim.g22.metropolis.einwohner.mutanten.Schurke;
 
 public class Syndikat {
-	private Schurke[] mitglieder;
-	private String name;
-	
-	public Syndikat(String name, Schurke... mitglieder) {
-		this.name = name;
-		this.mitglieder = mitglieder;
-	}
-	
-	public Schurke[] getMitglieder() {
-		return Arrays.copyOf(mitglieder, mitglieder.length);
-	}
-	
-	public String getName() {
-		return name;
-	}
+    private Schurke[] mitglieder;
+    private String name;
+
+    public Syndikat(String name, Schurke... mitglieder) {
+        this.name = name;
+        this.mitglieder = mitglieder;
+    }
+
+    public Schurke[] getMitglieder() {
+        return mitglieder;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
