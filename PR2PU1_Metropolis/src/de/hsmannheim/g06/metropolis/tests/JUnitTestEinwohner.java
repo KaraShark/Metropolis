44f47ce5b0730584317e1062253a77b4e1cbe964
Mustafa <Mustafa@192.168.0.150>
2018-12-02 21:07:27 +0100

    TestEinwohner dezent umgeändert

--- PR2PU1_Metropolis/src/de/hsmannheim/g06/metropolis/tests/JUnitTestEinwohner.java
+++ PR2PU1_Metropolis/src/de/hsmannheim/g06/metropolis/tests/JUnitTestEinwohner.java
@@ -10,7 +10,7 @@ import de.hsmannheim.g06.metropolis.einwohner.mutanten.*;
 public class JUnitTestEinwohner {
     public static final float PRAEZISION = 0.0001f;
 
-    Buerger mustafa = new Buerger("Mustafa", 22, 5);
+    Buerger buerger = new Buerger("Markus", 22, 287);
     Richter korrupterRichter = new Richter("Harald", 55, true);
     Richter nichtKorrupterRichter = new Richter("Uwe", 20, false);
     Schurke darklord = new Schurke("Dark-Lord", "Feuer", 100000);
@@ -22,9 +22,9 @@ public class JUnitTestEinwohner {
 
     @Test // teste die Daten der Buerger
     public void testeDatenVonBuerger() {
-        assertEquals("Mustafa", mustafa.getName());
-        assertEquals(22, mustafa.getAlter(), PRAEZISION);
-        assertEquals(5, mustafa.getEinkommen(), PRAEZISION);
+        assertEquals("Markus", buerger.getName());
+        assertEquals(22, buerger.getAlter(), PRAEZISION);
+        assertEquals(287, buerger.getEinkommen(), PRAEZISION);
     }
 
     @Test // teste kaempfe von Helden initiiert
